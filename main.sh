57e3e4781a5f069fd5faa137f353c917b6cb532d
TrollTeamTango <117917666+TrollTeamTango@users.noreply.github.com>
2022-11-13 17:38:37 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,54 +1,64 @@
-####
+cd $HOME
 sudo apt update
+echo downloading chrome
 wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+echo installing chrome
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
-
 ####sudo apt install python3-pip
+cd $HOME
+echo build-essential
 sudo apt-get install build-essential   -y
+echo installing python2-dev
 sudo apt-get install python2-dev   -y    
-
 #####distom
+cd $HOME
+echo installing distom
 git clone https://github.com/gdabah/distorm.git
 cd distorm
 sudo chmod 755 setup.py
 sudo python2 setup.py build install
-cd ..
 ######Yara
+cd $HOME
+echo installing Yara
 git clone --recursive https://github.com/VirusTotal/yara-python
 cd yara-python
 sudo python setup.py build
 sudo python setup.py install
-cd ..
 ######PyCripto
+cd $HOME
+echo installing PyCripto
 git clone https://github.com/pycrypto/pycrypto.git
 cd pycrypto
 sudo chmod 755 setup.py
 sudo ./setup.py install
-cd ..
 ######Volatility
+cd $HOME
+echo installing Volatility
 git clone https://github.com/volatilityfoundation/volatility.git
 cd volatility
 sudo chmod 755 setup.py
 sudo chmod 755 vol.py
-cd ..
-
 ######Python3
+cd $HOME
+echo installing Python3
 sudo apt install python3 
 ######Distom3
+cd $HOME
+echo installing DISTOM3
 git clone https://salsa.debian.org/debian/distorm3.git
 cd distorm3
 sudo chmod 755 setup.py
 sudo python3 setup.py build install   -y
 cd ..
 ######Volatility3
+cd $HOME
+echo installing Volatility3
 git clone https://github.com/volatilityfoundation/volatility3.git
 cd volatility3
-sudo chmod 755 setup.py    (or type sudo chmod 755 setup.py)
+sudo chmod 755 setup.py
 sudo chmod 755 vol.py
-cd ..
-#Installs Regripper 3.0
-#Installs latest RegRipper3.0
-#Installs win32-registry-perl
+cd $HOME
+echo Installing Regripper 3.0 and win32-registry-perl
 
 echo sudo is required
 echo git is required
