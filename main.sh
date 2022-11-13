dcc625ab62d1a96568bd8c8567208cce1443d101
TrollTeamTango <117917666+TrollTeamTango@users.noreply.github.com>
2022-11-13 17:06:31 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
 sudo apt update
-cd ~
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
 ####sudo apt install python3-pip
@@ -8,53 +7,44 @@ sudo apt-get install build-essential   -y
 sudo apt-get install python2-dev   -y    
 
 #####distom
-cd ~
 git clone https://github.com/gdabah/distorm.git
 cd distorm
 sudo chmod 755 setup.py
 sudo python2 setup.py build install
-
+cd ..
 ######Yara
-cd ~
 git clone --recursive https://github.com/VirusTotal/yara-python
 cd yara-python
 sudo python setup.py build
 sudo python setup.py install
-
+cd ..
 ######PyCripto
-cd ~
 git clone https://github.com/pycrypto/pycrypto.git
 cd pycrypto
 sudo chmod 755 setup.py
 sudo ./setup.py install
-
+cd ..
 ######Volatility
-cd ~
 git clone https://github.com/volatilityfoundation/volatility.git
 cd volatility
 sudo chmod 755 setup.py
 sudo chmod 755 vol.py
-
+cd ..
 
 ######Python3
-cd ~
 sudo apt install python3 
-
 ######Distom3
-cd ~
 git clone https://salsa.debian.org/debian/distorm3.git
 cd distorm3
 sudo chmod 755 setup.py
 sudo python3 setup.py build install
-
-
+cd ..
 ######Volatility3
-cd ~
 git clone https://github.com/volatilityfoundation/volatility3.git
 cd volatility3
 sudo chmod 755 setup.py    (or type sudo chmod 755 setup.py)
 sudo chmod 755 vol.py
-
+cd ..
 #Installs Regripper 3.0
 #Installs latest RegRipper3.0
 #Installs win32-registry-perl
